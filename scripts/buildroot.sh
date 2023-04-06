f727a6474412ad70528757062a0ad13459bdbb23
Alexander Eldeib <alexeldeib@gmail.com>
2023-04-06 03:31:59 +0000

    debug: buildroot paths and naming

--- scripts/buildroot.sh
+++ scripts/buildroot.sh
@@ -4,10 +4,11 @@ set -euxo pipefail
 sudo apt-get update && sudo apt-get install -yq tree libelf-dev
 
 REPO_ROOT=$(realpath $(dirname "${BASH_SOURCE[0]}")/..)
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+mkdir -p /tmp/buildroot || true
 
 root=$(pwd)/dist
-work=$(pwd)/$(mktemp -d work-XXXX)
+work=/tmp/buildroot
 pushd $work || exit 1
 
 function cleanup() {
@@ -15,15 +16,17 @@ function cleanup() {
   rm -r "$work" || true
 }
 
-trap cleanup EXIT
+# trap cleanup EXIT
 
 git clone https://github.com/buildroot/buildroot
 pushd buildroot
 git checkout 2023.02
-
-make BR2_EXTERNAL=$REPO_ROOT default_defconfig
-make BR2_EXTERNAL=$REPO_ROOT -j$(nproc)
 popd || exit 1
+popd || exit 1
+
+mkdir -p $REPO_ROOT/output
+make -C $work/buildroot O=$REPO_ROOT/output BR2_EXTERNAL=$REPO_ROOT fire_defconfig
+make -C $work/buildroot O=$REPO_ROOT/output -j$(nproc)
 
 tree buildroot/output/images
 
