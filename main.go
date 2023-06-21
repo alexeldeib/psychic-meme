b80217545d3dd523b9f8f7e8f9b4cca580ef279a
Alexander Eldeib <alexeldeib@gmail.com>
2023-06-21 02:30:19 +0200

    add main

--- main.go
+++ main.go
@@ -4,13 +4,19 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"os"
+	//"os"
+	"syscall"
 	"time"
 
 	"github.com/containerd/containerd"
+	"github.com/containerd/containerd/cio"
 	"github.com/containerd/containerd/leases"
 	"github.com/containerd/containerd/namespaces"
+	"github.com/containerd/containerd/oci"
 	"github.com/containerd/containerd/snapshots"
+	// "github.com/opencontainers/image-spec/identity"
+	specs "github.com/opencontainers/runtime-spec/specs-go"
+	"github.com/sanity-io/litter"
 )
 
 func main() {
@@ -29,7 +35,7 @@ func run() error {
 	defer client.Close()
 
 	manager := client.LeasesService()
-	l, err := manager.Create(ctx, leases.WithRandomID(), leases.WithExpiration(time.Second*10))
+	l, err := manager.Create(ctx, leases.WithRandomID(), leases.WithExpiration(time.Second*15))
 	if err != nil {
 		return fmt.Errorf("failed to create lease: %s", err)
 	}
@@ -37,67 +43,145 @@ func run() error {
 	// Update current context to add lease
 	ctx = leases.WithLease(ctx, l.ID)
 
-	// ctx, done, err := client.WithLease(ctx)
-	// if err != nil {
-	// 	return err
-	// }
-	// defer done(ctx)
-
-	snapshotter := client.SnapshotService("overlayfs")
-	tmpDir := "/tmp/unpack"
-
-	err = os.MkdirAll(tmpDir, 0666)
+	baseImage, err := client.Pull(ctx, "docker.io/alexeldeib/test:dev", containerd.WithPullUnpack)
 	if err != nil {
 		return err
 	}
+	log.Printf("Successfully pulled %s image\n", baseImage.Name())
 
-	defer os.RemoveAll(tmpDir)
+	userImage, err := client.Pull(ctx, "docker.io/library/hello-world:latest", containerd.WithPullSnapshotter("devmapper"), containerd.WithPullUnpack)
+	if err != nil {
+		return err
+	}
+	log.Printf("Successfully pulled %s image\n", userImage.Name())
 
-	layerPath := "/opt/fc/images/layer.tar" // just a path to layer tar file.
+	// if err := userImage.Unpack(ctx); err != nil {
+	// 	return err
+	// }
+	// log.Printf("Successfully unpacked %s image\n", userImage.Name())
+
+	desc, err := userImage.Config(ctx)
+	if err != nil {
+		return err
+	}
+	log.Printf("Successfully parsed image desc\n")
+
+	// diffs, err := userImage.RootFS(ctx)
+	// if err != nil {
+	// 	return err
+	// }
+	// chainID := identity.ChainID(diffs)
+
+	log.Printf("Successfully got desc id %s\n", desc.Digest.String())
+
+	cs := client.ContentStore()
+	info, err := cs.Info(ctx, desc.Digest)
+	if err != nil {
+		return err
+	}
+	log.Printf("Successfully parsed image info\n")
+
+	litter.Dump(info)
+
+	snapshotLabel, ok := info.Labels["containerd.io/gc.ref.snapshot.devmapper"]
+	if !ok {
+		return fmt.Errorf("failed to find snapshot label")
+	}
+
+	// baseImageSpec, err := baseImage.Spec(ctx)
+	// if err != nil {
+	// 	return err
+	// }
+
+	snapshotter := client.SnapshotService("devmapper")
+
 	noGcOpt := snapshots.WithLabels(map[string]string{
-		// "containerd.io/gc.root": time.Now().UTC().Format(time.RFC3339),
+		"containerd.io/gc.root": time.Now().UTC().Format(time.RFC3339),
 	})
 
-	mounts, err := snapshotter.Prepare(ctx, "foo", "", noGcOpt)
+	mounts, err := snapshotter.Prepare(ctx, "instance", snapshotLabel, noGcOpt)
 	if err != nil {
 		return fmt.Errorf("failed to prepare snapshot: %v", err)
 	}
+	defer func() {
+		if err := snapshotter.Remove(ctx, "instance"); err != nil {
+			log.Printf("failed to remove snapshot: %v", err)
+		}
+	}()
+	fmt.Printf("%#+v\n", mounts)
 
-	_, _ = layerPath, mounts
+	if len(mounts) != 1 {
+		return fmt.Errorf("expected 1 mount, found %d", len(mounts))
+	}
 
-	// fmt.Printf("%#+v\n", mounts)
+	ociMounts := []specs.Mount{
+		{
+			Source:      mounts[0].Source,
+			Destination: mounts[0].Source,
+			Type:        "bind",
+			Options:     []string{"rbind", "rw"},
+		},
+		{
+			Source:      "/dev/kvm",
+			Destination: "/dev/kvm",
+			Type:        "bind",
+			Options:     []string{"rbind", "rw"},
+		},
+	}
 
-	// if err := mount.All(mounts, tmpDir); err != nil {
-	// 	return fmt.Errorf("failed to mount all: %v", err)
-	// }
-	// defer mount.UnmountAll(tmpDir, 0)
+	container, err := client.NewContainer(
+		ctx,
+		"vmm",
+		containerd.WithNewSnapshot("vmm-snapshot", baseImage),
+		containerd.WithNewSpec(
+			oci.WithDefaultSpec(),
+			oci.WithImageConfig(baseImage),
+			oci.WithDefaultUnixDevices,
+			oci.WithAllDevicesAllowed,
+			oci.WithHostDevices,
+			oci.WithPrivileged,
+			oci.WithMounts(ociMounts),
+		),
+	)
+	if err != nil {
+		return err
+	}
+	defer container.Delete(ctx, containerd.WithSnapshotCleanup)
+	log.Printf("Successfully created container with ID %s and snapshot with ID vmm-snapshot", container.ID())
 
-	// layer, err := os.Open(layerPath)
-	// if err != nil {
-	// 	return fmt.Errorf("failed to open layer: %v", err)
-	// }
+	task, err := container.NewTask(ctx, cio.NewCreator(cio.WithStdio))
+	if err != nil {
+		return err
+	}
+	defer task.Delete(ctx)
 
-	// _, err = archive.UnpackLayer(tmpDir, layer, nil) // unpack into layer location
-	// if err != nil {
-	// 	return fmt.Errorf("failed to unpack layer: %v", err)
-	// }
+	// make sure we wait before calling start
+	exitStatusC, err := task.Wait(ctx)
+	if err != nil {
+		fmt.Println(err)
+	}
 
-	// // at this point you'd *think* tmpDir would have the unpacked layer.
-	// // but it doesn't. it's empty. why?
-	// // we took an empty parent layer, prepared it, and mounted the snapshot.
-	// // we mounted the snapshotter dir over the target dir.
-	// // we end up unpacking into the snapshot dir instead (I think?).
-	// // otherwise we'd see the mounted contents of the parent layers,
-	// // while our writes would still go to the active snapshot.
+	// call start on the task to execute the redis server
+	if err := task.Start(ctx); err != nil {
+		return err
+	}
 
-	// if err := snapshotter.Commit(ctx, "bar", "foo", noGcOpt); err != nil {
-	// 	return fmt.Errorf("failed to commit snapshot: %v", err)
-	// }
+	// sleep for a lil bit to see the logs
+	time.Sleep(10 * time.Second)
 
-	// at this point the original snapshot is gone, but the new one could be seen in storage
-	// ctr -n example snapshot ls
-	// to clean up:
-	// ctr -n example snapshot rm foo
+	// kill the process and get the exit status
+	if err := task.Kill(ctx, syscall.SIGTERM); err != nil {
+		return err
+	}
+
+	// wait for the process to fully exit and print out the exit status
+
+	status := <-exitStatusC
+	code, _, err := status.Result()
+	if err != nil {
+		return err
+	}
+	fmt.Printf("vmm exited with status: %d\n", code)
 
 	return nil
 }
